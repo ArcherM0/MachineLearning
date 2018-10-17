85fa6fc7adb2791b197e463c1f76f17cb3a695b2
Archer Mo <archermo512@gmail.com>
2018-10-17 16:11:07 -0700

    Update LogisticReg.py

--- LogisticReg.py
+++ LogisticReg.py
@@ -3,7 +3,24 @@ import numpy as np
 imporr random
 import matplotlib.pyplot as plt 
 
-# Define the cost function
+# Define the hypothesis function
+def sigmoid(z):
+    return 1 / (1 + np.exp(-z))
 
+# Define the Gradient Descent
+def Grandient(x, y, alpha, theta, m):
+    # hypothesis
+    h = sigmoid(np.dot(x, theta))
+    # cost function
+    cost = (np.dot(-y.T, np.log(h)) - np.dot((1-y).T, np.log(1-h))) / m
+    # gradient
+    gradient = np.dot(x.T, (h - y)) / m
+    # update theta
+    theta = theta - alpha * gradient
+
+
+    
+           
+    
 
     
