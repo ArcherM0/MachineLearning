c11a6a76b5ad69170c85054ee1c41dca1da449dd
Archer Mo <archermo512@gmail.com>
2018-11-22 12:08:12 -0800

    Update NeuralNetworks.py

--- NeuralNetworks.py
+++ NeuralNetworks.py
@@ -1,16 +1,18 @@
-''' 2-layer model'''
+'''
+Neural Networks
+'''
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 np.random.seed(1)
 
-def initialize_parameters(layer_dims):
+def _initialize_parameters(layer_dims):
   '''
-  Input:
+  Inputs:
   layer_dims -- ndarray, contains the dimensions of each layer in the network
   
-  output:
+  return:
   parameters -- dictionary, contains 'W1', 'b1',..., 'WL', 'bL'
                 Wl -- sparse matrix, shape (layer_dims[l], layer_dims[l-1]
                 bl -- ndarray, shape (layer_dims[l], 1)
@@ -26,6 +28,28 @@ def initialize_parameters(layer_dims):
     parameters['b' + str(l)] = np.zeros((layer_dims[l], 1))
     
   return parameters  
+
+
+def _forward_propagation(X, parameters):
+  '''
+  Implement forward propagation 
+  
+  inputs:
+  X -- tranning data, sparse matrix, shape (n_features, n_samples)
+  parameters -- output of _initialize_parameters
+  
+  output
+  AL -- last post-activation value
+  caches -- list of caches containing:
+            every cache of linear_activation_forward() (there are L-1 of them, indexed from 0 to L-1)
+  '''
+  
+  caches = []
+  A = X
+  L = len(parameters) // 2
+  
+  for l in range(1, L):
+    
 
 
 
