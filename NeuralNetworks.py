ea43178dd8da0f2aea5375aa68776ff25ab8ee99
Archer Mo <archermo512@gmail.com>
2018-11-22 11:07:44 -0800

    Update NeuralNetworks.py
    wrote a function that initializes the weight matrix and bias vector of each layer in the network

--- NeuralNetworks.py
+++ NeuralNetworks.py
@@ -1,1 +1,35 @@
+''' 2-layer model'''
+
 import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(1)
+
+def initialize_parameters(layer_dims):
+  '''
+  Input:
+  layer_dims -- ndarray, contains the dimensions of each layer in the network
+  
+  output:
+  parameters -- dictionary, contains 'W1', 'b1',..., 'WL', 'bL'
+                Wl -- sparse matrix, shape (layer_dims[l], layer_dims[l-1]
+                bl -- ndarray, shape (layer_dims[l], 1)
+
+  '''
+  
+  np.random.seed(3)
+  parameters = {}
+  L = len(layer_dims)  # number of layers in the network
+  
+  for l in range (1, L):
+    parameters['W' + str(l)] = np.random.randn(layer_dim[l], layer_dims[l-1]) * 0.01
+    parameters['b' + str(l)] = np.zeros((layer_dims[l], 1))
+    
+  return parameters  
+
+
+
+
+
+  
+  
